2333197b99db6d2ae98d9635ed5006b7b6c02136
sydnyyy <nayoung8142@naver.com>
2025-07-19 22:31:25 +0900

    Initial commit

---
+++ src/test/java/com/exchangerateapi/ExchangeRateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exchangerateapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeRateApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
