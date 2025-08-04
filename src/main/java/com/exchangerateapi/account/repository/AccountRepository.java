16629e3cf643a5d6f1d928de4d5330cb24191db7
sydnyyy <nayoung8142@naver.com>
2025-08-04 19:16:39 +0900

    chore: Account 관련 기능 제거

--- src/main/java/com/exchangerateapi/account/repository/AccountRepository.java
+++
@@ -1,6 +0,0 @@
-package com.exchangerateapi.account.repository;
-
-import com.exchangerateapi.account.entity.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account, Long> { }
