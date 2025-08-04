98e139b804adec8a9477c2348c1b3912d6fc39ed
sydnyyy <nayoung8142@naver.com>
2025-08-05 01:14:42 +0900

    build: DB 설정 제거

--- src/main/java/com/exchangerateapi/ExchangeRateApiApplication.java
+++ src/main/java/com/exchangerateapi/ExchangeRateApiApplication.java
@@ -3,10 +3,8 @@ package com.exchangerateapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 @EnableFeignClients(basePackages = "com.exchangerateapi")
 public class ExchangeRateApiApplication {
 
